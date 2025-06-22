67241019247ad1f2d7be22eecde81d8da6467f7f
Mahmoud-Ahmed-Awad <mahmodawadahmed@gmail.com>
2025-06-23 01:31:58 +0300

    fix stripe

--- server/controllers/weebhooks.js
+++ server/controllers/weebhooks.js
@@ -65,7 +65,7 @@ export const stripeWebhooks = async (request, response) => {
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(
+    event = Stripe.webhooks.constructEvent(
       request.body,
       sig,
       process.env.STRIPE_WEBHOOK_SECRET
